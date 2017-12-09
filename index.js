e6946f2cdb0e5a7f26c1a52de79f6e0c21f1beef
AjSchaff <aschaff4@gmail.com>
2017-12-08 22:25:48 -0500

    initial setup of app

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
