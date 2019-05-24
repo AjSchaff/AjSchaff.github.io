005a9fb6e66aa4eac9a020a969182b34f1b5c3fa
Andrew Schaffner <aschaff4@gmail.com>
2019-05-24 13:34:32 -0400

    Added CNAME File

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-
-// Enabling click events for all nav-bar links
-let anchorLinks = document.querySelectorAll('a[href^="#"]');
-
-for (let item of anchorLinks) {
-    item.addEventListener('click', (e) => {
-        console.log("this is what e is: ", e);
-        console.log('this was the click, :', item);
-    })
-}
-
-=======
 
 // Enabling click events for all nav-bar links
 let anchorLinks = document.querySelectorAll('a[href^="#"]');
@@ -28,5 +15,4 @@ for (let item of anchorLinks) {
     });
 }
 
->>>>>>> 2fdebacfe08df3d5de4dc5cf14b899d850846f72
 console.log(anchorLinks);
